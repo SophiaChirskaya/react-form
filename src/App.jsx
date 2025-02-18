c019818a215f1f20111aae00729b989620e9cd19
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-18 17:36:59 +0100

    [feat] array & two consts

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const titles = [
+    "Alla Scoperta delle Meraviglie Nascoste",
+    "Come Viaggiare con un Budget Ridotto",
+    "Diario di Viaggio: Esperienze Indimenticabili",
+    "I Migliori Posti per un Weekend Romantico",
+    "Viaggiare da Soli: Consigli e Suggerimenti",
+    "Le Città più Fotogeniche del Mondo",
+    "Come Organizzare un Road Trip Perfetto",
+    "Viaggi Eco-Sostenibili: Dove Andare e Cosa Fare"
+  ];
+
+  const [title, setTitle] = useState(initialBlog);
+  const [newTitle, setNewTitle] = useState('');
+
+  function renderTitles() {
+    return titles.map(title => (
+
+    ))
+  }
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <input type='text' value={title}
+    onChange={e => { setTitle(e.target.value) }} />
+    <p>Il titolo {title}</p>
+
     </>
   )
 }
